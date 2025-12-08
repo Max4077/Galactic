d0f69d4a06837b47c24e6f9e384f2b2e6ea07653
Max4077 <mande264@students.kennesaw.edu>
2025-12-07 21:16:46 -0500

    oopsie

--- Assets/_scripts/EnemyController.cs
+++ Assets/_scripts/EnemyController.cs
@@ -5,38 +5,57 @@ public class EnemyController : MonoBehaviour
     public Rigidbody rb;
     public Transform player;
     private Vector3 playerPosition;
+    LayerMask enemyMask, playerMask;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        enemyMask = LayerMask.GetMask("Enemy");
+        playerMask = LayerMask.GetMask("Player");
+
+        //Debug.Log(AimedAtPlayer());
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        //playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
         Chase(); 
+        
     }
 
     private void FixedUpdate()
     {
-        
+        //Debug.Log(Target());
         
     }
 
     void Chase()
     {
-        //this.transform.LookAt(player.position);
-        //rb.AddForce(player.position * 0.5f);
+        float playerDistance = Vector3.Distance(player.position, transform.position);
 
-        this.transform.LookAt(playerPosition);
-        this.transform.position = Vector3.MoveTowards(this.transform.position, playerPosition, 20f);
+        if(playerDistance > 10f)
+        {
+            transform.LookAt(player.position);
+            rb.AddForce(transform.forward * 0.5f);
+        }
+
+        if(Target() == 1)
+        {
+            Fire();
+        }
     }
     void Dodge()
     {
 
+    }
+
+    void Fire()
+    {
+        Debug.Log("Pew Pew!!");
     }
 
     float UtilityFunction(GameState currentState)
@@ -48,10 +67,29 @@ public class EnemyController : MonoBehaviour
         return score;
     }
 
-    //bool AimedAtPlayer()
-    //{
-    //    RaycastHit hit;
-    //}
+    int Target()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemyMask))
+        {
+            //Debug.Log("Hit an Enemy");
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            return 0;
+        }
+        else if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, playerMask))
+        {
+            //Debug.Log("Hit a Player");
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            return 1;
+        }
+        else
+        {
+            //Debug.Log("Hit nothing :(");
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+        return -1;
+    }
 }
 
 public class GameState
