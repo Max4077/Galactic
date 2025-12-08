400e631644d2654a2ded28ab7ad24825c74b0684
Max4077 <mande264@students.kennesaw.edu>
2025-12-07 21:54:26 -0500

    promgress

--- Assets/_scripts/EnemyController.cs
+++ Assets/_scripts/EnemyController.cs
@@ -4,39 +4,107 @@ public class EnemyController : MonoBehaviour
 {
     public Rigidbody rb;
     public Transform player;
-    private Vector3 playerPosition;
+    private Vector3 currentTarget;
+    
+    LayerMask enemyMask, playerMask;
+
+    float attackCooldown = 1f;
+    float lastAttackTime;
+
+    int mode = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+
+        enemyMask = LayerMask.GetMask("Enemy");
+        playerMask = LayerMask.GetMask("Player");
+
+        //Debug.Log(AimedAtPlayer());
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
-        Chase(); 
-    }
-
-    private void FixedUpdate()
-    {
-        
-        
+        if(mode == 0)
+        {
+            Chase();
+        }
+        else
+        {
+            Dodge();
+        }
     }
 
     void Chase()
     {
-        //this.transform.LookAt(player.position);
-        //rb.AddForce(player.position * 0.5f);
+        float playerDistance = Vector3.Distance(player.position, transform.position);
 
-        this.transform.LookAt(playerPosition);
-        this.transform.position = Vector3.MoveTowards(this.transform.position, playerPosition, 20f);
+        if(playerDistance > 10f)
+        {
+            transform.LookAt(player.position);
+            rb.AddForce(transform.forward * 5f);
+        }
+
+        if (Time.time - lastAttackTime < attackCooldown) return;
+        lastAttackTime = Time.time;
+
+        if (Target() == 1)
+        {
+            Fire();
+        }
     }
+
     void Dodge()
     {
+        float objectDistance = Vector3.Distance(currentTarget, transform.position);
 
+        if (objectDistance < 20f)
+        {
+            //transform.LookAt(player.position);
+            //rb.AddForce(-transform.forward * 5f);
+            Debug.Log("In dodge mode");
+        }
+
+        mode = 0; 
+    }
+
+    void Fire()
+    {
+        Debug.Log("Pew Pew!!");
+    }
+
+    int Target()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemyMask))
+        {
+            //Debug.Log("Hit an Enemy");
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            return 0;
+        }
+        else if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, playerMask))
+        {
+            //Debug.Log("Hit a Player");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            return 1;
+        }
+        else
+        {
+            //Debug.Log("Hit nothing :(");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+        return -1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        mode = 1;
+        currentTarget = other.transform.position;
+        Debug.Log("Triggered by" + other.gameObject.name);
     }
 
     float UtilityFunction(GameState currentState)
@@ -47,11 +115,6 @@ public class EnemyController : MonoBehaviour
 
         return score;
     }
-
-    //bool AimedAtPlayer()
-    //{
-    //    RaycastHit hit;
-    //}
 }
 
 public class GameState
