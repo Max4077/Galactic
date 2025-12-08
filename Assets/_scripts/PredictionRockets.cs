259cb4937de05504dabf95a16b230313df6f1117
rjone300 <61524630+rjone300@users.noreply.github.com>
2025-12-08 00:40:51 -0500

    Rockets are all coded, collision needs to be checked and fixed
    Done:
    Rocket code, it will track the player at a delayed rate, make collision if possible, deal damage,
    PredictionRocket code, it will guess a spot within five units of where the player is, go to that spot, and then blow itself up.,
    Prefabs have been made for both, with models,
    
    Problems:
    Collision is screwy, it keeps thinking that it hits the player no matter what it collides with,
    
    To Be Done:
    Instantiation Function, which is really just one line of code that you all have to put into your attack functions

--- Assets/_scripts/PredictionRockets.cs
+++ Assets/_scripts/PredictionRockets.cs
@@ -1,48 +1,38 @@
-using System.Runtime.CompilerServices;
-using System.Collections;
-using UnityEditor;
 using UnityEngine;
 
-public class Rockets : MonoBehaviour
+public class PredictionRockets : MonoBehaviour
 {
     public Transform player;
     private Vector3 waypoint;
     private Rigidbody rb;
+    
 
-    private float timer = 0;
-    [SerializeField] float targetUpdateTime;
     [SerializeField] private float speed;
 
     void Start()
     {
         player = ArrowPlayerController.Singleton.transform;
         rb = GetComponent<Rigidbody>();
+        waypoint = player.position + new Vector3(Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2));
     }
 
-    void Update()
-    {
-        if (timer >= 0)
-        {
-            timer -= Time.deltaTime;
-        }
-        if (timer <= 0)
-        {
-            waypoint = player.position;
-            timer = targetUpdateTime;
-
-        }
-    }
 
     private void FixedUpdate()
     {
         transform.LookAt(waypoint);
-        rb.AddForce(transform.forward*speed, ForceMode.Acceleration);
+        rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
+
+        if (this.gameObject.transform.position == waypoint)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
     void OnCollisionEnter(Collision other)
     {
-        
-        if (other.gameObject.CompareTag("Player"))
+
+        if (other.gameObject.tag == "Player")
         {
             ArrowPlayerController playerTarget = other.gameObject.GetComponent<ArrowPlayerController>();
             Debug.Log("Collided with player");
